ac5fc4e0f62c1e41e97120d2ace1cc0094a24a03
Eleni <31955471+Eleni21K@users.noreply.github.com>
2018-06-07 09:05:40 +0100

    Add sphere

--- procedural_spiral.py
+++ procedural_spiral.py
@@ -1,7 +1,12 @@
 # procedural modelling
 
-from maya import cmds
+import maya.cmds as cmds
 
 # create a sphere
+cmds.polySphere( n='mySphere', sx=5, sy=5, r=1)
 
 # duplicate n times
+
+# select spheres 
+
+# move spheres in circle
