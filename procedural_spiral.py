1146d514041f45c628c0ff5592134b989cdae60f
Eleni <31955471+Eleni21K@users.noreply.github.com>
2018-06-08 09:24:35 +0100

    Duplicate spheres

--- procedural_spiral.py
+++ procedural_spiral.py
@@ -6,7 +6,10 @@ import maya.cmds as cmds
 cmds.polySphere( n='mySphere', sx=5, sy=5, r=1)
 
 # duplicate n times
-
+for i in range(0,50):
+    cmds.duplicate('mySphere')
+   # cmds.move()
+   # cmds.rotate(0,i*20,0)
 # select spheres 
 
 # move spheres in circle
